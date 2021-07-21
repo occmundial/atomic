1bdebeeb153b713586d44fde2d0494fcad673072
Luis Fernando Díaz <ldiaz@occ.com.mx>
2021-07-21 15:33:46 -0500

    Corrección para pruebas

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,11 @@ module.exports = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/lib/components/$1',
+    '^@/tokens/(.*)$': '<rootDir>/lib/tokens/$1',
+    '^@/hooks/(.*)$': '<rootDir>/lib/hooks/$1',
+    '^@/styles/(.*)$': '<rootDir>/styles/$1'
   }
 }
