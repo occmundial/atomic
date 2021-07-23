87f0ac42d739ac35c1e24f73bded9e75fa46d8cb
Luis Fernando Díaz <ldiaz@occ.com.mx>
2021-07-23 03:24:14 -0500

    Se migra OrderBy

---
+++ lib/components/OrderBy/styles.ts
@@ -0,0 +1,53 @@
+import { createUseStyles } from 'react-jss'
+
+import spacing from '@/tokens/spacing'
+import colors from '@/tokens/colors'
+import iconSizes from '@/tokens/iconSizes'
+import grid from '@/tokens/grid'
+
+export default createUseStyles({
+  wrap: {
+    display: 'inline-block',
+    position: 'relative'
+  },
+  button: {
+    cursor: 'pointer'
+  },
+  icon: {
+    marginLeft: spacing.xTiny,
+    marginTop: 2,
+    width: iconSizes.tiny,
+    height: iconSizes.tiny
+  },
+  cardWrap: {
+    position: 'absolute',
+    top: '100%',
+    right: 0,
+    opacity: 0,
+    marginTop: -spacing.small,
+    transition: '0.3s all',
+    zIndex: 1,
+    pointerEvents: 'none'
+  },
+  card: {
+    padding: spacing.small,
+    [`@media screen and (min-width:${grid.sm}px)`]: {
+      padding: spacing.base
+    }
+  },
+  show: {
+    opacity: 1,
+    marginTop: 0,
+    pointerEvents: 'auto'
+  },
+  option: {
+    cursor: 'pointer',
+    whiteSpace: 'nowrap',
+    '&:not(:last-child)': {
+      paddingBottom: spacing.tiny
+    }
+  },
+  link: {
+    color: colors.textLink
+  }
+})
