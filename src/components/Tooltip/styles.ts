2a71e0d42da53e16e906edf3829be8893460ccd8
Isai Ibarra Ibarra <iibarra@occ.com.mx>
2023-05-23 10:48:33 -0600

    fix: Change default property values and remove unnecessary themes

--- src/components/Tooltip/styles.ts
+++ src/components/Tooltip/styles.ts
@@ -1,18 +1,6 @@
 import { createUseStyles } from 'react-jss'
 import colors from '@/tokens/colors'
-const {
-  successLight,
-  successText,
-  errorLight,
-  errorText,
-  warningLight,
-  warningText,
-  infoLight,
-  infoText,
-  info,
-  white,
-  grey900
-} = colors
+const { infoLight, infoText, info, white, grey900 } = colors
 
 export default createUseStyles({
   activator: {
@@ -24,18 +12,6 @@ export default createUseStyles({
     fontSize: '13px',
     lineHeight: '16px',
     maxWidth: '260px'
-  },
-  success: {
-    background: successLight,
-    color: successText
-  },
-  error: {
-    background: errorLight,
-    color: errorText
-  },
-  warning: {
-    background: warningLight,
-    color: warningText
   },
   info: {
     background: infoLight,
