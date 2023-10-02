548757606c3e437f3bdc537b3a744b90559b46c2
Aaron Damian <56703361+a-damian@users.noreply.github.com>
2023-10-02 13:35:59 -0600

    refactor: Remove unused values in styles

--- lib/components/Logo/styles.ts
+++ lib/components/Logo/styles.ts
@@ -1,19 +1,5 @@
 import { createUseStyles } from 'react-jss'
 
-import { base } from '@/tokens/icons'
-import occHorizontal from '@/tokens/icons/occHorizontal'
-import occVertical from '@/tokens/icons/occVertical'
-import occIcon from '@/tokens/icons/occIcon'
-import { LogoProps } from '.'
-import colors from '@/tokens/colors'
-
-const logoColors = {
-  black: colors.ink,
-  grey: colors.inkLight,
-  white: colors.white,
-  blue: colors.prim
-}
-
 export default createUseStyles({
   logo: {
     boxSizing: 'border-box',
