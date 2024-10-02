96956dd65f59609afc49f5a51b12693f42343a1d
emmanuel-yanez-occ <ryanez@occ.com.mx>
2024-10-02 12:07:12 -0600

    fix: Menu dropdown classes

--- src/components/MenuDropdown/index.tsx
+++ src/components/MenuDropdown/index.tsx
@@ -21,7 +21,7 @@ export default function MenuMDX({ placement, darkMode = false, className }) {
           <NavAvatarButton
             darkMode={darkMode}
             mini
-            className={className.button}
+            className={className?.button}
             photo="https://i.pravatar.cc/300"
             onClick={avatarButtonHandler}
           />
