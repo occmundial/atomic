fc5aecca2ea8e9135d4eaf2a8fe23abb2a43b3b7
Miguel Malagon <malagon@hey.com>
2025-03-19 11:17:46 -0600

    feat: Include tokens

--- lib/tokens/ct.ts
+++ lib/tokens/ct.ts
@@ -3,5 +3,6 @@ import fonts from './ct/fonts.json'
 import borderRadius from './ct/borderRadius.json'
 import shadows from './ct/shadows.json'
 import spacing from './ct/spacing.json'
+// Include the rest of the tokens
 
 export { colors, fonts, borderRadius, shadows, spacing }
