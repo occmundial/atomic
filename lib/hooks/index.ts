73411fb93797f5af3c1d0e87cd94f5531a9a4c83
Alexia García Peñafiel <agarcia@occ.com.mx>
2025-03-21 13:36:27 -0600

    feat: Add hook to get brand value

--- lib/hooks/index.ts
+++ lib/hooks/index.ts
@@ -1,4 +1,5 @@
 import useAtomic from './useAtomic'
+import useBrand from './useBrand'
 import useEventListener from './useEventListener'
 import useIsClient from './useIsClient'
 import usePrevious from './usePrevious'
@@ -8,6 +9,7 @@ import useIsMounted from './useIsMounted'
 
 export {
   useAtomic,
+  useBrand,
   useEventListener,
   useIsClient,
   usePrevious,
