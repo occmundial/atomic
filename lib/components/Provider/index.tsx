6727760c9f90370d8803e14086698ab46c687469
Alexia García Peñafiel <agarcia@occ.com.mx>
2025-03-25 16:51:28 -0600

    feat: Set default value for brand

--- lib/components/Provider/index.tsx
+++ lib/components/Provider/index.tsx
@@ -38,6 +38,7 @@ const AtomicProvider = ({ data, children }: AtomicProviderProps) => {
     iconsPath: '',
     translateIconsV2: false,
     iconsPrefix: 'atomic',
+    brand: 'OCC',
     ...data
   })
   const valueRef = useRef<Partial<AtomicData>>()
