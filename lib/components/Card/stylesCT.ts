a2a55cf1855e8175d57840a0cb59d1170b0fab29
DaniCuevas <jcuevas@occ.com.mx>
2025-04-03 14:01:49 -0600

    fix: Remove useless styles to ct card

--- lib/components/Card/stylesCT.ts
+++ lib/components/Card/stylesCT.ts
@@ -1,5 +1,4 @@
 import colors from '@/tokens/ct/colors.json'
-import shadowsOCC from '@/tokens/shadows'
 import spacing from '@/tokens/ct/spacing.json'
 import grid from '@/tokens/grid'
 
@@ -9,17 +8,13 @@ const styles = {
     borderRadius: spacing['size-2'],
     padding: spacing['size-5'],
     position: 'relative',
-    transition: '0.3s box-shadow, 0.3s transform',
     [`@media screen and (min-width:${grid.xs}px)`]: {
       padding: spacing['size-4']
     }
   },
   cardNoPadding: {
     extend: 'card',
-    padding: 0,
-    [`@media screen and (min-width:${grid.sm}px)`]: {
-      padding: 0
-    }
+    padding: 0
   },
   flat: {
     borderWidth: 1,
@@ -27,24 +22,11 @@ const styles = {
     borderColor: colors['option-card']['border']['default'],
     '&:hover': {
       borderColor: colors['option-card']['border']['hover']
-    },
-    zIndex: 'auto'
-  },
-  rest: {
-    boxShadow: shadowsOCC.lvl0,
-    zIndex: 'auto'
-  },
-  raised: {
-    boxShadow: shadowsOCC.lvl4,
-    zIndex: 4
-  },
-  raisable: {
-    '&:hover': {
-      boxShadow: shadowsOCC.lvl4,
-      transform: `translateY(-${spacing['size-2']})`,
-      zIndex: 4
     }
-  }
+  },
+  rest: {},
+  raised: {},
+  raisable: {}
 }
 
 export default styles
