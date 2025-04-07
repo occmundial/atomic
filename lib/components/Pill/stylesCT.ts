b77f7749a28b39f630e54b55e5f7de3e7b43bf06
Andrea Esquivel Galan <aesquivel@occ.com.mx>
2025-04-07 12:57:18 -0600

    fix: Documentation

--- lib/components/Pill/stylesCT.ts
+++ lib/components/Pill/stylesCT.ts
@@ -1,8 +1,4 @@
-import colors from '@/tokens/ct/colors.json'
 import spacing from '@/tokens/ct/spacing.json'
-import fonts from '@/tokens/ct/fonts.json'
-import { objectToFontValue } from '@/utils/font'
-import { CT as brand } from '@/constants/index'
 
 const styles = {
   container: {
@@ -20,21 +16,11 @@ const styles = {
     float: 'left'
   },
   bottom: {
-    height: 21,
-    display: 'flex',
-    alignItems: 'center',
-    marginTop: spacing['size-2']
+    display: 'none'
   },
-  validAssistiveText: {
-    color: colors.text.default //Pendiente de ajustar para CT
-  },
-  errorAssistiveText: {
-    color: colors.text.error //Pendiente de ajustar para CT
-  },
-  errorIcon: {
-    marginTop: -2,
-    marginRight: spacing['size-1']
-  },
+  validAssistiveText: {},
+  errorAssistiveText: {},
+  errorIcon: {},
   stackGroup: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -46,10 +32,7 @@ const styles = {
     gap: spacing['size-4']
   },
   label: {
-    //Clase pendiente de ajustar para CT
-    font: objectToFontValue(fonts['text-field-label'], brand),
-    color: colors.text.default,
-    transition: '0.3s all'
+    display: 'none'
   }
 }
 
