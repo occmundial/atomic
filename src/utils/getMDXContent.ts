fe667e9e25be9a9074c97ce4343edc84c85d0d36
DaniCuevas <jcuevas@occ.com.mx>
2025-04-09 12:47:44 -0600

    feat:  Toggle CT styles

--- src/utils/getMDXContent.ts
+++ src/utils/getMDXContent.ts
@@ -6,8 +6,9 @@ import glob from 'fast-glob'
 import { CT } from '@/constants/index'
 
 const showCT = process.env.ATOMIC_BRAND === CT
-const excludedTitles = showCT ? ['Alert', 'Button', 'Pill'] : ['AlertCT', 'ButtonCT', 'PillCT']
-
+const excludedTitles = showCT
+  ? ['Alert', 'Button', 'Pill', 'Toggle']
+  : ['AlertCT', 'ButtonCT', 'PillCT', 'ToggleCT']
 
 export async function getMdxContent(source) {
   const contentGlob = `${source}/**/*.mdx`
