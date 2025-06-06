a3b0e105a8ebba46dac06be1965b13b4d64ba2ea
Andrea Esquivel Galan <aesquivel@occ.com.mx>
2025-06-06 13:22:39 -0600

    feat: Add StylesCT Autocomplete

---
+++ lib/components/Autocomplete/stylesCT.ts
@@ -0,0 +1,19 @@
+import spacing from '@/tokens/ct/spacing.json'
+
+const styles = {
+  autoComplete: {
+    position: 'relative'
+  },
+  droplist: {
+    position: 'absolute',
+    zIndex: 2,
+    top: `calc(100% + ${spacing['size-2']})`,
+    left: 0,
+    width: '100%'
+  },
+  pushDroplist: {
+    top: `calc(100% - 28px)`
+  }
+}
+
+export default styles
